ae225df258e7e96033e0e4586358aeef0dbbf33c
rattus_ratgirl <100981706+josh-kynaston@users.noreply.github.com>
2025-11-16 19:51:10 +0000

    add Serilog logging in HelloModule
    Added Serilog as a dependency and implemented logging in HelloModule to record command usage.
    
    Signed-off-by: rattus_ratgirl <100981706+josh-kynaston@users.noreply.github.com>

--- RatBot.Interactions/HelloModule.cs
+++ RatBot.Interactions/HelloModule.cs
@@ -1,14 +1,23 @@
 ﻿using Discord;
 using Discord.Interactions;
+using Serilog;
 
 namespace RatBot.Interactions;
 
 public sealed class HelloModule : SlashCommandBase
 {
+    private readonly ILogger _logger;
+    
+    public HelloModule(ILogger logger)
+    {
+        _logger = logger.ForContext<HelloModule>();
+    }
+    
     [SlashCommand("hello", "Says hello!")]
     [RequireUserPermission(GuildPermission.SendMessages)]
     public async Task HelloAsync()
     {
+        _logger.Information("Received hello command from {User}", Context.User.Username);
         await RespondAsync($"Hello, {Context.User.Mention}!", ephemeral: true);
 
         // Alternative way to defer and send a followup message in the channel
