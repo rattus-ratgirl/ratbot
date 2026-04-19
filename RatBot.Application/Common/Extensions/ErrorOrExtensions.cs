faff2bf25b269211d22d3c020c05c97d9e06165b
= <220043304+rattus-ratgirl@users.noreply.github.com>
2026-04-19 07:07:51 +0100

    ur a monad & ud better start acting like one [skip ci]

--- RatBot.Application/Common/Extensions/ErrorOrExtensions.cs
+++ RatBot.Application/Common/Extensions/ErrorOrExtensions.cs
@@ -2,12 +2,83 @@ namespace RatBot.Application.Common.Extensions;
 
 public static class ErrorOrExtensions
 {
-    /// <summary>
-    ///     Ensures that the source value satisfies the predicate.
-    /// </summary>
-    /// <param name="source">The source error-or value.</param>
-    /// <param name="predicate">The predicate function to validate the source value.</param>
-    /// <returns>An <see cref="ErrorOr{TValue}" /> containing the validated value or an error if the predicate fails.</returns>
-    public static ErrorOr<T> Ensure<T>(this ErrorOr<T> source, Func<T, ErrorOr<Success>> predicate) =>
-        source.Then(value => predicate(value).Then(_ => value));
+    extension<T>(ErrorOr<T> source)
+    {
+        public ErrorOr<TResult> Select<TResult>(Func<T, TResult> selector) =>
+            source.IsError
+                ? source.Errors
+                : selector(source.Value);
+
+        public ErrorOr<TResult> SelectMany<TInner, TResult>(
+            Func<T, ErrorOr<TInner>> bind,
+            Func<T, TInner, TResult> project)
+        {
+            if (source.IsError)
+                return source.Errors;
+
+            ErrorOr<TInner> inner = bind(source.Value);
+
+            return inner.IsError
+                ? inner.Errors
+                : project(source.Value, inner.Value);
+        }
+
+        public async Task<ErrorOr<TResult>> SelectMany<TInner, TResult>(
+            Func<T, Task<ErrorOr<TInner>>> bind,
+            Func<T, TInner, TResult> project)
+        {
+            if (source.IsError)
+                return source.Errors;
+
+            ErrorOr<TInner> inner = await bind(source.Value).ConfigureAwait(false);
+
+            return inner.IsError
+                ? inner.Errors
+                : project(source.Value, inner.Value);
+        }
+    }
+
+    extension<T>(Task<ErrorOr<T>> sourceTask)
+    {
+        public async Task<ErrorOr<TResult>> Select<TResult>(Func<T, TResult> selector)
+        {
+            ErrorOr<T> source = await sourceTask.ConfigureAwait(false);
+
+            return source.IsError
+                ? source.Errors
+                : selector(source.Value);
+        }
+
+        public async Task<ErrorOr<TResult>> SelectMany<TInner, TResult>(
+            Func<T, ErrorOr<TInner>> bind,
+            Func<T, TInner, TResult> project)
+        {
+            ErrorOr<T> source = await sourceTask.ConfigureAwait(false);
+
+            if (source.IsError)
+                return source.Errors;
+
+            ErrorOr<TInner> inner = bind(source.Value);
+
+            return inner.IsError
+                ? inner.Errors
+                : project(source.Value, inner.Value);
+        }
+
+        public async Task<ErrorOr<TResult>> SelectMany<TInner, TResult>(
+            Func<T, Task<ErrorOr<TInner>>> bind,
+            Func<T, TInner, TResult> project)
+        {
+            ErrorOr<T> source = await sourceTask.ConfigureAwait(false);
+
+            if (source.IsError)
+                return source.Errors;
+
+            ErrorOr<TInner> inner = await bind(source.Value).ConfigureAwait(false);
+
+            return inner.IsError
+                ? inner.Errors
+                : project(source.Value, inner.Value);
+        }
+    }
 }
